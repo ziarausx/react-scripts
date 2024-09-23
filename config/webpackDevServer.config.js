f74ea5a40f1a4ab7608c5b1ae58b3ed789590e41
Michal Ziarkiewicz <michal.ziarkiewicz@iamip.com>
2024-09-23 13:43:30 +0200

    Fixed config

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -100,7 +100,7 @@ module.exports = function (proxy, allowedHost) {
       publicPath: paths.publicUrlOrPath.slice(0, -1),
     },
 
-    https: getHttpsConfig(),
+    // https: getHttpsConfig(),
     host,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
